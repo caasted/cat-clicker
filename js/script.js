e06dad2d0171dd5c79ea5f9629582bb9b91c2a78
Chris Aasted <caasted@gmail.com>
2016-10-31 16:14:36 -0700

    feat: Add second cat with separate click counter

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
-var count = 0;
+var count1 = 0;
+var count2 = 0;
 
 $(document).ready(function () {
-    var $counter = $('#counter');
-    $(".photo").click(function () {
-        count = count + 1;
-        $counter.text(count);
+    var $counter1 = $('#counter1');
+    $(".photo#cat1").click(function () {
+        count1 = count1 + 1;
+        $counter1.text(count1);
+    });
+})
+
+$(document).ready(function () {
+    var $counter2 = $('#counter2');
+    $(".photo#cat2").click(function () {
+        count2 = count2 + 1;
+        $counter2.text(count2);
     });
 })
