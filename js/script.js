d120fdfe318f7821b25ff4afdd62873ca8297e52
Chris Aasted <caasted@gmail.com>
2016-11-07 14:20:36 -0800

    feat: Add functionality for delux version

--- js/script.js
+++ js/script.js
@@ -1,16 +1,33 @@
-var counters[7] = 0;
-
-var cats = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven'];
+var cats = ['Fluffy', 'Ralph', 'Bob', 'Tom', 'Sven', 'Lucious', 'Frank'];
+var clicks = [0, 0, 0, 0, 0, 0, 0];
+var activeCat = 0;
 
 $(document).ready(function () {
 
-    var $counter = $('#counter1');
-    var $photoID1 = $('#cat1')
+    var $catName = $('#cat-name');
+    var $catPhoto = $('#cat-photo');
+    var $selector = $('#selector');
+    var $counter = $('#counter');
 
-    $(".photo#cat").click(function () {
-        count1 = count1 + 1;
-        $counter1.text(count1);
-        $photoID1.text('You clicked on ' + cat1);
+    for (var i = 0; i < cats.length; i++) {
+    	var elem = document.createElement('div');
+    	elem.textContent = cats[i];
+		
+    	elem.addEventListener('click', (function(clickedCat) {
+    		return function() {
+	       		$catName.text(cats[clickedCat]);
+    			$catPhoto.attr('src', "images/cat" + clickedCat + ".jpg");
+        		$counter.text(clicks[clickedCat]);
+        		activeCat = clickedCat;
+    		};
+    	})(i));
+
+    	$selector.append(elem);
+    }
+
+    $("#cat-photo").click(function (event) {
+    	clicks[activeCat] += 1;
+		$counter.text(clicks[activeCat]);        
     });
-})
 
+})
