04f8608b6c631e2f68cf4950c36d4013a89882da
Chris Aasted <caasted@gmail.com>
2016-11-08 09:47:23 -0800

    feat: Add localStorage and refactor to MVO format

--- js/script.js
+++ js/script.js
@@ -1,33 +1,118 @@
-var cats = ['Fluffy', 'Ralph', 'Bob', 'Tom', 'Sven', 'Lucious', 'Frank'];
-var clicks = [0, 0, 0, 0, 0, 0, 0];
-var activeCat = 0;
+$(function () {
 
-$(document).ready(function () {
+    var model = {
+        activeCat: 0, 
+        init: function() {
+            if (!localStorage.catclicker) {
+                localStorage.catclicker = JSON.stringify([]);
+                octopus.addAllCats();
+            }
+        }, 
+        add: function(obj) {
+            var data = JSON.parse(localStorage.catclicker);
+            data.push(obj);
+            localStorage.catclicker = JSON.stringify(data);
+        }, 
+        get: function() {
+            return JSON.parse(localStorage.catclicker);
+        }, 
+        incrementClicks: function() {
+            var data = JSON.parse(localStorage.catclicker);
+            for (var i = 0; i < data.length; i++) {
+                if (data[i]['id'] == model.activeCat) {
+                    data[i]['clicks'] = data[i]['clicks'] + 1;
+                    break;
+                }
+            }
+            localStorage.catclicker = JSON.stringify(data);
+        }, 
+        getActive: function() {
+            return model.activeCat;
+        }, 
+        setActive: function(newCat) {
+            model.activeCat = newCat;
+        }
+    };
 
-    var $catName = $('#cat-name');
-    var $catPhoto = $('#cat-photo');
-    var $selector = $('#selector');
-    var $counter = $('#counter');
+    var octopus = {
+        addAllCats: function() {
+            var cats = ['Fluffy', 'Ralph', 'Bob', 'Tom', 'Sven', 'Lucious', 'Frank'];
+            for (var i = 0; i < cats.length; i++) {
+                model.add({
+                    id: i, 
+                    name: cats[i], 
+                    photo: "images/cat" + i + ".jpg", 
+                    clicks: 0
+                });
+            }
+        }, 
+        getCats: function() {
+            return model.get();
+        }, 
+        getCatByID: function(catID) {
+            var data = model.get();
+            for (var i = 0; i < data.length; i++) {
+                if (data[i].id == catID) {
+                    return data[i];
+                }
+            }
+            return 'Cat ID Not Found';
+        }, 
+        updateClicks: function(catID) {
+            model.incrementClicks(catID);
+        }, 
+        init: function() {
+            model.init();
+            view.init();
+        }, 
+        getActive: function() {
+            return model.getActive();
+        }, 
+        setActive: function(newCat) {
+            model.setActive(newCat);
+        }
+    };
 
-    for (var i = 0; i < cats.length; i++) {
-    	var elem = document.createElement('div');
-    	elem.textContent = cats[i];
-		
-    	elem.addEventListener('click', (function(clickedCat) {
-    		return function() {
-	       		$catName.text(cats[clickedCat]);
-    			$catPhoto.attr('src', "images/cat" + clickedCat + ".jpg");
-        		$counter.text(clicks[clickedCat]);
-        		activeCat = clickedCat;
-    		};
-    	})(i));
+    var view = {
+        init: function() {
+            view.renderSelector();
+            view.renderViewer(0);
 
-    	$selector.append(elem);
-    }
+            $("#cat-viewer").click(function (event) {
+                octopus.updateClicks();
+                view.renderViewer(octopus.getActive());
+            });
+        }, 
+        renderSelector: function() {
+            var data = octopus.getCats()
+            var $selector = $('#cat-selector');
+            for (var i = 0; i < data.length; i++) {
+                var elem = document.createElement('div');
+                elem.textContent = data[i].name;
+                
+                elem.addEventListener('click', (function(i) {
+                    return function() {
+                        octopus.setActive(i);
+                        view.renderViewer(i);
+                    };
+                })(i));
 
-    $("#cat-photo").click(function (event) {
-    	clicks[activeCat] += 1;
-		$counter.text(clicks[activeCat]);        
-    });
+                $selector.append(elem);
+            }
+        }, 
+        renderViewer: function(catID) {
+            cat = octopus.getCatByID(catID);
+            var elem = document.getElementById('cat-viewer');
+            elem.innerHTML = '<h3 id="cat-name">' + 
+                cat.name + 
+                '</h3><img class="photo" id="cat-photo" src="' + 
+                cat.photo + '">' + 
+                '<h3 id="counter-header">Clicker Counter: ' + 
+                cat.clicks + '</h3>';
+        }
+    };
 
-})
+    octopus.init();
+});
+
+
